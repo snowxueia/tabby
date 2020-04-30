34b8053f8f217e54bd6d61d91f6606fb28f92273
snowxueia <56488060+snowxueia@users.noreply.github.com>
2020-04-30 17:00:33 -0400

    Update firebase.ts

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,12 +1,13 @@
+// for security, We delete some information in this part.
 const config = {
-    apiKey: "AIzaSyCafIruN5ziOora3R4RRQ8ph32jBZslJrs",
-    authDomain: "log-in-page-cd261.firebaseapp.com",
-    databaseURL: "https://log-in-page-cd261.firebaseio.com",
-    projectId: "log-in-page-cd261",
-    storageBucket: "log-in-page-cd261.appspot.com",
-    messagingSenderId: "112832152187",
-    appId: "1:112832152187:web:9f9f37fc6fed30af1bd8de",
-    measurementId: "G-80BWQW3F28"
+    apiKey: 
+    authDomain: 
+    databaseURL: 
+    projectId: ,
+    storageBucket: 
+    messagingSenderId: 
+    appId: 
+    measurementId: 
   }
 
   export default config
